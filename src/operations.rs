46698be92183f7564c6da587f062d610b2d996ac
Finley Gibson <f.j.gibson@exeter.ac.uk>
2025-06-10 17:00:37 +0100

    added task syncrnoisation using threads

--- src/operations.rs
+++ src/operations.rs
@@ -1,10 +1,24 @@
 use crate::config::Config;
 use crate::set::{global_theme, wallpaper};
 use crate::theme::Style;
+use std::thread;
 
 pub fn set(style: &Style) {
-    let _ = global_theme::set(&style.desktop_theme);
-    let _ = wallpaper::set(&style.wallpaper);
+    // TODO: remove need to clone here?
+    let theme = style.desktop_theme.clone();
+    let wallpaper = style.wallpaper.clone();
+
+    let theme_handle = thread::spawn(move || {
+        let _ = global_theme::set(&theme);
+    });
+
+    let wallpaper_handle = thread::spawn(move || {
+        let _ = wallpaper::set(&wallpaper);
+    });
+
+    // Wait for both to finish
+    let _ = theme_handle.join();
+    let _ = wallpaper_handle.join();
 }
 
 pub fn toggle(config: Config) {
