70e1b83cc1152724975dbad0208dff17e3fdccb0
Finley Gibson <f.j.gibson@exeter.ac.uk>
2025-06-16 13:43:08 +0100

    impl from_string for theme

--- src/theme.rs
+++ src/theme.rs
@@ -1,6 +1,7 @@
 use clap::{Error, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use std::str::FromStr;
 
 /// Theme types, also used as part of the cli subcommand structure
 #[derive(Subcommand, Debug, Serialize, Deserialize, PartialEq)]
@@ -9,6 +10,18 @@ pub enum Theme {
     Light,
     #[command(about = "Set theme to Dark")]
     Dark,
+}
+
+impl FromStr for Theme {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "light" => Ok(Theme::Light),
+            "dark" => Ok(Theme::Dark),
+            _ => Err(()),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
